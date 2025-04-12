7925ea91199c62c8388f025fe3fee0a2714ef673
shruti0781 <shruti0781.becse24@chitkara.edu.in>
2025-04-12 14:39:53 +0530

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,17 @@
-const express= require('express');// require import karne keh liya kam attah hai.
-const app= express();
+const express = require('express');
+const app = express();
 const http = require("http");
 const socketio = require("socket.io");
 const server = http.createServer(app);
-const io= socketio(server);//calling the server
-app.set("view engine","ejs");
-app.set("express.static(path.join(__dirname,"public"));
-app.get("\",function(req,res){
-        res.send("hey");
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res) {
+  res.send("hey");
 });
-app.listen(3000);
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
