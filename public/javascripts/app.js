efb6c3745192e9dff7de2bcf753ed1b39b0511ea
shruti0781 <shruti0781.becse24@chitkara.edu.in>
2025-04-19 13:18:16 +0000

    trial committing my files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,27 @@
-
 const express = require('express');
 const app = express();
-const http = require("http");
-const socketio = require("socket.io");
-const path = require("path");
+const socketio = require('socket.io');
+const http = require('http');
+const path = require('path');
+
 const server = http.createServer(app);
 const io = socketio(server);
-
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "../../public")));
-app.use(express.static(path.join(__dirname, "../../")));
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../../index.html"));
+app.use(express.static(path.join(__dirname, "public")));
+io.on("connection", function(socket){
+  socket.on("sendlocation", function(data){
+    io.emit("sendlocation", {id: socket.id, data: data});
+  })
+  socket.on("disconnect", function(){
+    io.emit("sendlocation", {id: socket.id, data: null});
+  })
+  console.log("connected");
+  socket.on("disconnect", function(){
+    console.log("disconnected");
+  });
+})
+app.get("/", function(req, res){
+res.render("index.ejs");
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
-});
+server.listen(3000);
