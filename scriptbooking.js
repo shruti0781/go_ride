1c528211a5ac5d7eae8ce145e2036623fc32a100
shruti0781 <shruti0781.becse24@chitkara.edu.in>
2025-04-26 14:18:57 +0530

    Update scriptbooking.js
    the text from from and to show on next page

--- scriptbooking.js
+++ scriptbooking.js
@@ -1,6 +1,12 @@
 function handleContinue() {
-    
-    window.location.href = "buses.html";
-    return false;
-  }
-  
+  const fromLocation = document.querySelector('input[placeholder="Enter departure city"]').value;
+  const toLocation = document.querySelector('input[placeholder="Enter destination city"]').value;
+
+  // Save in localStorage
+  localStorage.setItem('fromLocation', fromLocation);
+  localStorage.setItem('toLocation', toLocation);
+
+  window.location.href = "buses.html";
+  return false;
+}
+
